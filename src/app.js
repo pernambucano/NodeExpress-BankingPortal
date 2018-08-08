433f1c18031f8f046979be0196cd5287d8a4d290
jose.paulo.fernandes <jose.paulo.fernandes@accenture.com>
2018-08-08 17:46:16 -0300

    fix errors

--- src/app.js
+++ src/app.js
@@ -7,11 +7,8 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req, res) {
-    res.render('index', {title: 'Index'});
-});
+app.get('/', (req, res) => res.render('index', {title: 'Index'}));
 
-app.listen(3000);
-console.log('PS Project Running on port 3000!');
+app.listen(3000, () => console.log('PS Project Running on port 3000!'));
