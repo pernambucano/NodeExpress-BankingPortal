efa630bcb9ae627d8f32cec31587498abfcac222
jose.paulo.fernandes <jose.paulo.fernandes@accenture.com>
2018-08-09 11:38:13 -0300

    feature: remove redundancies in app.js.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
+const data = require('./data');
 
 const app = express();
 
@@ -8,13 +9,11 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded(extended=true));
+app.use(express.urlencoded(extended = true));
 
-const accountData = fs.readFileSync('src/json/accounts.json', "utf8");
-const accounts = JSON.parse(accountData);
-
-const userData = fs.readFileSync('src/json/users.json', 'utf8');
-const users = JSON.parse(userData);
+const accounts = data.accounts;
+const users = data.users;
+const writeJSON = data.writeJSON;
 
 app.get('/', (req, res) => res.render('index', {
     title: 'Account Summary',
@@ -39,27 +38,35 @@ app.get('/profile', (req, res) => res.render('profile', {
 
 app.get('/transfer', (req, res) => res.render('transfer'));
 
-app.post('/transfer', (req, res) => {    
-    accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount; 
+app.post('/transfer', (req, res) => {
+    accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
 
     accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount);
 
     const accountsJSON = JSON.stringify(accounts);
 
-    fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
+    writeJSON();
 
-    res.render('transfer', {message: "Transfer Completed"});
+    res.render('transfer', {
+        message: "Transfer Completed"
+    });
 });
 
-app.get('/payment', (req, res) => res.render('payment', {account: accounts.credit}));
+app.get('/payment', (req, res) => res.render('payment', {
+    account: accounts.credit
+}));
 
 app.post('/payment', (req, res) => {
     accounts.credit.balance = accounts.credit.balance - req.body.amount;
     accounts.credit.available = parseInt(accounts.credit.available) + parseInt(req.body.amount);
     const accountsJSON = JSON.stringify(accounts);
-    const myPath = path.join(__dirname, 'json/accounts.json');
-    fs.writeFileSync(myPath, accountsJSON, 'utf8'); 
-    res.render('payment', {message: "Payment Successful", account: accounts.credit});
+
+    writeJSON();
+
+    res.render('payment', {
+        message: "Payment Successful",
+        account: accounts.credit
+    });
 });
 
 app.listen(3000, () => console.log('PS Project Running on port 3000!'));
